78bda613e48682deefc2e773f6f73236074a90cc
Antonia Blair <antoniablair@gmail.com>
2016-12-01 00:42:50 -0500

    Fix error

---
+++ RecipeApp/utils/RecipeData.js
@@ -0,0 +1,30 @@
+var Promise = require('bluebird');
+
+var RecipeData = {
+    get: function(searchTerm) {
+        return new Promise(function (resolve, reject) {
+
+            var xmlhttp = new XMLHttpRequest();
+            var url = ('http://www.recipepuppy.com/api/?q=' + searchTerm);
+
+
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                   if (xmlhttp.status == 200) {
+                       var data = JSON.parse(xmlhttp.responseText);
+                       resolve(data['results']);
+                   }
+                   else {
+                    //   todo: better error handling
+                       var result = new Error();
+                       reject(result);
+                   }
+                }
+            };
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+            }
+        )}
+}
+
+module.exports = RecipeData;
