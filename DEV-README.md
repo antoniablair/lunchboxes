2f7064d1acb0d2f2879073d51871661281fe744e
Antonia Blair <antoniablair@gmail.com>
2016-12-26 13:17:49 -0500

    Add dev instructions md
    - Update the recipeData.js file (commented out vanilla javascript version)

---
+++ DEV-README.md
@@ -0,0 +1,3 @@
+- Run "webpack" to create new bundle.js when developing
+- Run "npm start" for development server
+- Displays at localhost:8080/
--- RecipeApp/utils/RecipeData.js
+++ RecipeApp/utils/RecipeData.js
@@ -15,6 +15,7 @@ var RecipeData = {
                      resolve(data['results']);
                  },
                  error: function(err) {
+                   console.log('the error is ' + err);
                    var result = new Error();
                    reject(result);
                  },
@@ -23,5 +24,35 @@ var RecipeData = {
      }
 }
 
+//var RecipeData = {
+//    get: function(searchTerm) {
+//        return new Promise(function (resolve, reject) {
+//
+//            var xmlhttp = new XMLHttpRequest();
+//            var url = ('http://www.recipepuppy.com/api/?q=' + searchTerm);
+//
+//
+//            xmlhttp.onreadystatechange = function() {
+//                if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+//                   if (xmlhttp.status == 200) {
+//                       var data = JSON.parse(xmlhttp.responseText);
+//                       resolve(data['results']);
+//                   }
+//                   else {
+//                    //   todo: better error handling
+//                       var result = new Error();
+//                       reject(result);
+//                   }
+//                }
+//            };
+//            xmlhttp.open("GET", url, true);
+//            xmlhttp.send();
+//            }
+//        )}
+//}
+
 module.exports = RecipeData;
+
+
+
 
